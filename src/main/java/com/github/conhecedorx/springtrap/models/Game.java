5a14519a74024f65bfe99c3439a3c5fce949c498
ConhecedorX~ <Lucastation2@gmail.com>
2024-09-26 00:14:18 -0300

    .

--- src/main/java/com/github/conhecedorx/springtrap/models/Game.java
+++ src/main/java/com/github/conhecedorx/springtrap/models/Game.java
@@ -18,58 +18,6 @@ public class Game {
     @Enumerated(EnumType.STRING)
     private Genero genero;
     private String player;
-
-    public Game(long id, String nomeJogo, double nota, Genero genero, String player) {
-        this.id = id;
-        this.nomeJogo = nomeJogo;
-        this.nota = nota;
-        this.genero = genero;
-        this.player = player;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNomeJogo() {
-        return nomeJogo;
-    }
-
-    public void setNomeJogo(String nomeJogo) {
-        this.nomeJogo = nomeJogo;
-    }
-
-    public double getNota() {
-        return nota;
-    }
-
-    public void setNota(double nota) {
-        this.nota = nota;
-    }
-
-    public Genero getGenero() {
-        return genero;
-    }
-
-    public void setGenero(Genero genero) {
-        this.genero = genero;
-    }
-
-    public String getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(String player) {
-        this.player = player;
-    }
-
-    public Game() {
-
-    }
 }
 
 // Game game = new Game(id = 1, nomeJogo = "CrashBandicoot", nota = 8.6, genero = "RPG" , player = "Lucas" );
